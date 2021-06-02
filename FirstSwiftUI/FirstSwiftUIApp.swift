d41f5a0dd4bae9ab0b3bfe7b34986cbf039e6277
Bilgehan KAYA <bilgehankaya79@gmail.com>
2021-06-02 22:56:42 +0300

    Initial Commit

---
+++ FirstSwiftUI/FirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIApp.swift
+//  FirstSwiftUI
+//
+//  Created by Bilgehan KAYA on 2.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
