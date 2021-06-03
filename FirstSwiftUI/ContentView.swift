255d94d4319a07b29ddbaaa594bf97681813657c
Bilgehan KAYA <bilgehankaya79@gmail.com>
2021-06-03 05:23:00 +0300

    AppFinished

--- FirstSwiftUI/ContentView.swift
+++ FirstSwiftUI/ContentView.swift
@@ -1,16 +1,53 @@
-//
-//  ContentView.swift
-//  FirstSwiftUI
-//
-//  Created by Bilgehan KAYA on 2.06.2021.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading){
+            VStack {
+                Text("Hello, swift")
+                    .padding(.all)
+                    .font(.largeTitle)
+                Text("Hello, world!")
+                    .padding(.top, 20)
+                    .padding(.bottom, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    .font(.system(size: 35))
+                Text("Hello, world!")
+                    .padding()
+                    .font(.footnote)
+            }
+            HStack{
+                Text("Hello, swift")
+                    .padding(.all)
+                    .font(.largeTitle)
+                Spacer()
+                Text("Hello, world!")
+                    .padding(.top, 20)
+                    .padding(.bottom, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    .font(.system(size: 35))
+                Spacer()
+                Text("Hello, world!")
+                    .padding()
+                    .font(.footnote)
+                    .foregroundColor(Color(red: 0.0, green: 0.0, blue: 1.0, opacity: 1.0))
+            }
+            ZStack{
+                Image("darth_vader1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("Darth Vader!")
+                    .padding(.top, 190)
+                    .padding(.bottom, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    .font(.system(size: 35))
+                    .foregroundColor(.red)
+            }
+            
+            Button(action: {
+                //Button a basılınca çalışacak kod
+            }, label: {
+                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+            })
+            
+        }
     }
 }
 
